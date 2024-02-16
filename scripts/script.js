41c773b3a2950b43d07f20abf319b5a05a084255
Perps <josephzhao2008@gmail.com>
2024-02-16 13:16:38 -0800

    Add navigation page to access individual sections and slides

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,27 @@
-let currentSlide = 1;
+let currentSlide = 1
 
 function incrementSlide(incrementAmount) {
     currentSlide = Math.max(currentSlide + incrementAmount, 1);
-    document.getElementById("page-number").textContent = "#" + currentSlide;
+    document.getElementById("slide-number").textContent = "#" + currentSlide;
 }
 
+function setSlide(slideNumber) {
+    currentSlide = Math.max(slideNumber, 1);
+    document.getElementById("slide-number").textContent = "#" + currentSlide;
+}
+
+function changeSection(pageName, slideNumber = 1) {
+    localStorage.setItem("slide", slideNumber);
+    window.location.href = "html/" + pageName + ".html";
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("Setting current slide to " + localStorage.getItem("slide"));
+    setSlide(localStorage.getItem("slide"))
+})
+
+// detect for key presses
 document.addEventListener('keydown', (event) => {
-    if (event.key === "ArrowRight") incrementSlide(1);
-    if (event.key === "ArrowLeft") incrementSlide(-1);
+    if (event.key === "ArrowRight" || event.key === 'd') incrementSlide(1);
+    if (event.key === "ArrowLeft"  || event.key === 'a') incrementSlide(-1);
 });
