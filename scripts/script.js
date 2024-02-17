becb957e159e3083a22c2066077ff59872b2ccc8
Perps <josephzhao2008@gmail.com>
2024-02-17 15:59:43 -0800

    Add animation between slides and create dummy presentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,59 @@
-let currentSlide = 1
+let currentSlide = 1;
 
 function incrementSlide(incrementAmount) {
     setSlide(currentSlide + incrementAmount);
 }
 
+function setSlideVisibilities(slides) {
+    slides[currentSlide - 1].classList.remove("before-shown");
+    slides[currentSlide - 1].classList.remove("after-shown");
+    for (let i = 0; i < currentSlide - 1; i++) {
+        slideClassList = slides[i].classList;
+        slideClassList.remove("before-shown");
+        slideClassList.add("after-shown");
+    }
+    for (let i = currentSlide; i < slides.length; i++) {
+        slideClassList = slides[i].classList;
+        slideClassList.remove("after-shown");
+        slideClassList.add("before-shown");
+    }
+}
+
 function setSlide(slideNumber) {
     let backButton = document.getElementById("back-button");
     let forwardButton = document.getElementById("forward-button");
-    let lastSlide = document.getElementsByClassName("slide").length
+    let slides = document.getElementsByClassName("slide");
+    let lastSlide = slides.length;
 
     currentSlide = Math.min(Math.max(slideNumber, 1), lastSlide);
     document.getElementById("slide-number").textContent = "#" + currentSlide;
-    document.documentElement.style.setProperty("--progress-bar-progress", '' + (currentSlide - 1) / (lastSlide - 1) * 100)
+    document.documentElement.style.setProperty("--progress-bar-progress", '' + (currentSlide - 1) / (lastSlide - 1) * 100);
+    setSlideVisibilities(slides);
     
+    backButton.disabled = false;
+    forwardButton.disabled = false;
     // disable the back button on the first slide...
     if (currentSlide <= 1) {
         backButton.disabled = true;
     }
     // and disable the front button on the last slide
-    else if (currentSlide >= lastSlide) {
+    if (currentSlide >= lastSlide) {
         forwardButton.disabled = true;
-    } 
-    else {
-        backButton.disabled = false;
-        forwardButton.disabled = false;
     }
 }
 
 function changeSection(pageName, slideNumber = 1) {
+    let path = window.location.pathname;
+    let page = path.split('/').pop();
+    if (pageName + ".html" == page) {
+        setSlide(slideNumber);
+        return;
+    }
     localStorage.setItem("slide", slideNumber);
+    if (window.location.pathname.includes("html/")) {
+        window.location.href = pageName + ".html"
+        return;
+    }
     window.location.href = "html/" + pageName + ".html";
 }
 
