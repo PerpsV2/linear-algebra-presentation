7d6a58272a0b9425b7b9fdf85ee97500541d9e18
Perps <josephzhao2008@gmail.com>
2024-02-19 10:28:07 -0800

    Organize three.js demo code

--- dist/scripts/vector-demo.js
+++ dist/scripts/vector-demo.js
@@ -1,36 +1,56 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
+var demoContainer = document.getElementById("vector-demo-container");
+var demoCanvas = document.getElementById("vector-demo-canvas");
+var zoomSlider = document.getElementById("zoom");
+
+const frustumAspectRatio = demoCanvas.offsetWidth / demoCanvas.offsetHeight;
 const displayScale = 0.6;
+var frustumSize = 2;
 
-let demoContainer = document.getElementById("vector-demo-container");
-let demoCanvas = document.getElementById("vector-demo-canvas");
+var scene;
+var camera;
+var renderer;
+var sceneObjects = [];
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-    canvas: demoCanvas
-});
+// initialize scene, camera, renderer and all the objects which will be rendered
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.OrthographicCamera(frustumAspectRatio * frustumSize / -2, frustumAspectRatio * frustumSize / 2, 
+    frustumSize / 2, frustumSize / -2, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer( {canvas: demoCanvas} );
+    renderer.setClearColor(0x252525, 1);
+    renderer.setSize(window.innerWidth * displayScale, window.innerHeight * displayScale);
+    demoContainer.appendChild(renderer.domElement);
 
-renderer.setSize(window.innerWidth * displayScale, window.innerHeight * displayScale);
-demoContainer.appendChild(renderer.domElement);
+    const basicMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff } );
+    const greenMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00 } );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial( {color: 0xFFFFFF } );
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    camera.position.z = 5;
 
-camera.position.z = 5;
+    createCubeMesh(basicMaterial, 1, 1, 1);
+    createCubeMesh(greenMaterial, 1, 2, 0.5);
+    animate();
+}
 
-let xRotationSlider = document.getElementById("x-rotation-slider");
-let yRotationSlider = document.getElementById("y-rotation-slider");
-let zRotationSlider = document.getElementById("z-rotation-slider");
+// create the mesh of a cube
+function createCubeMesh(material, length, width, height) {
+    const geometry = new THREE.BoxGeometry(length, width, height);
+    const mesh = new THREE.Mesh(geometry, material)
+    sceneObjects.push(mesh);
+    scene.add(mesh);
+}
 
 // render loop
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += parseFloat(xRotationSlider.value);
-    cube.rotation.y += parseFloat(yRotationSlider.value);
-    cube.rotation.z += parseFloat(zRotationSlider.value);
+    frustumSize = zoomSlider.value;
+    console.log(frustumSize);
+    camera.left = frustumAspectRatio * frustumSize / -2;
+    camera.right = frustumAspectRatio * frustumSize / 2;
+    camera.top = frustumSize / 2;
+    camera.bottom = frustumSize / -2;
+    camera.updateProjectionMatrix();
     renderer.render(scene, camera);
 }
-animate();
+init();
