2182a94cda3d83d3dc392b336eb0f5077d8473e7
Perps <josephzhao2008@gmail.com>
2024-02-19 18:54:06 -0800

    Add custom shader material to apply uniform transformation matrix

--- dist/scripts/vector-demo.js
+++ dist/scripts/vector-demo.js
@@ -17,22 +17,67 @@ var camera;
 var renderer;
 var sceneObjects = [];
 
+function vertexShader() {
+    return `
+    uniform mat4 transformation;
+    void main() {
+        vec4 modelViewPosition = modelViewMatrix * transformation * vec4(position, 1.0);
+        gl_Position =  projectionMatrix * modelViewPosition;
+    }`;
+}
+
+function fragmentShader() {
+    return `
+    void main() {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    `
+}
+
+function lineFragmentShader() {
+    return `
+    void main() {
+        gl_FragColor = vec4(0.4, 0.4, 0.4, 1.0);
+    }
+    `
+}
+
 // initialize scene, camera, renderer and all the objects which will be rendered
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.OrthographicCamera(frustumAspectRatio * frustumSize / -2, frustumAspectRatio * frustumSize / 2, 
     frustumSize / 2, frustumSize / -2, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer( {canvas: demoCanvas} );
+    renderer = new THREE.WebGLRenderer({canvas: demoCanvas});
     renderer.setClearColor(0x252525, 1);
     renderer.setSize(window.innerWidth * displayScale, window.innerHeight * displayScale);
     demoContainer.appendChild(renderer.domElement);
 
     camera.position.z = 5;
 
-    const axisMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-    const lineMaterial = new THREE.LineBasicMaterial( {color: 0xffffff, transparent: true, opacity: 0.3} );
+    const matrix = new THREE.Matrix4();
+    matrix.set (1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1);
 
-    sceneObjects = sceneObjects.concat(createGridMesh(lineMaterial, 1, 20));
+    let uniforms = {
+        transformation: { type: 'mat4', value: matrix }
+    };
+
+    const axisMaterial = new THREE.ShaderMaterial({
+        fragmentShader: fragmentShader(),
+        vertexShader: vertexShader(),
+        uniforms: uniforms
+    });
+
+    const lineMaterial = new THREE.ShaderMaterial({
+        transparency: true,
+        fragmentShader: lineFragmentShader(),
+        vertexShader: vertexShader(),
+        uniforms: uniforms
+    });
+
+    sceneObjects = sceneObjects.concat(createGridMesh(lineMaterial, cellSize, gridSize));
     sceneObjects.push(createCubeMesh(axisMaterial, gridSize * 2, axisWidth, axisWidth));
     sceneObjects.push(createCubeMesh(axisMaterial, axisWidth, gridSize * 2, axisWidth));
     sceneObjects.push(createCubeMesh(axisMaterial, axisWidth, axisWidth, gridSize * 2));
